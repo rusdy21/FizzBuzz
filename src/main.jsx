29e534659bd58877a8d767f121413332f1889763
Rusdy Masykuri <rusdy.masykuri@datacomm.co.id>
2023-11-27 05:22:47 +0700

    Commit Awal aplikasi FizzBuzz

---
+++ src/main.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+function CounterDisplay({ count }) {
+  if (count === 0) {
+    return <p>{count}</p>;
+  }
+ 
+  if (count % 5 === 0 && count % 7 === 0) {
+    return <p>FizzBuzz</p>;
+  }
+ 
+  if (count % 5 === 0) {
+    return <p>Fizz</p>;
+  }
+ 
+  if (count % 7 === 0) {
+    return <p>Buzz</p>;
+  }
+  
+  return <p>{count}</p>;
+}
+
+function IncreaseButton({ increase }) {
+  return (
+    <div>
+      <button onClick={increase}>+ increase</button>
+    </div>
+  );
+}
+
+function ResetButton({ reset }) {
+  return (
+    <div>
+      <button onClick={reset}>- reset</button>
+    </div>
+  );
+}
+
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    };
+
+    // binding event handler
+    this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
+    this.onResetEventHandler = this.onResetEventHandler.bind(this);
+
+  }
+
+  /*event count +1 */
+  onIncreaseEventHandler() {
+    this.setState((previousState) => {
+      return {
+        count: previousState.count + 1
+      }
+    })
+  }
+
+  onResetEventHandler() {
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
+  }
+s
+  render() {
+    return (
+      <div>
+        <IncreaseButton increase={this.onIncreaseEventHandler} />
+        <CounterDisplay count={this.state.count} />
+        <ResetButton reset={this.onResetEventHandler} />
+      </div>
+    );
+  }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<CounterApp />);
